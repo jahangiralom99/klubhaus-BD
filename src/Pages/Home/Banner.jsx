3a1c4130c975427a209581ef542934942ef6af62
jahangir <jahangiralomtuf99@gmail.com>
2024-07-02 10:04:36 +0600

    added banner section new

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -62,18 +62,21 @@ const Banner = () => {
             </div>
           </SwiperSlide>
           <SwiperSlide>
-          <div className="relative">
-          <img className="h-screen md:h-full object-cover" src={banner2} />
-          <div className="space-y-4 md:block absolute left-10 top-[400px] md:top-[200px] lg:top-[250px]">
-            <h1 className="text-3xl font-semibold text-white"> EID STYLES </h1>
-            <p className="text-xl text-white">The Season's latest</p>
-            <Link to="/card">
-            <button className="px-6 text-white py-3 border-2 border-white text-sm font-bold hover:bg-white hover:text-black">
-              SHOP
-            </button>
-            </Link>
-          </div>
-        </div>
+            <div className="relative">
+              <img className="h-screen md:h-full object-cover" src={banner2} />
+              <div className="space-y-4 md:block absolute left-10 top-[400px] md:top-[200px] lg:top-[250px]">
+                <h1 className="text-3xl font-semibold text-white">
+                  {" "}
+                  EID STYLES{" "}
+                </h1>
+                <p className="text-xl text-white">The Season's latest</p>
+                <Link to="/card">
+                  <button className="px-6 text-white py-3 border-2 border-white text-sm font-bold hover:bg-white hover:text-black">
+                    SHOP
+                  </button>
+                </Link>
+              </div>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
